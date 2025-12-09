6793fcee7a13217a56887121e8b50220615ecde7
MonkeyDRemi <remi.vavichandran2@gmail.com>
2025-12-09 00:37:21 -0500

    Implementation Structures Boot Sector et Directory Entry

--- src/fat32/mod.rs
+++ src/fat32/mod.rs
@@ -33,3 +33,45 @@ impl<D: Disk> Fat32<D> {
         Ok(Fat32 { disk })
     }
 }
+
+
+#[repr(packed)]
+#[allow(dead_code)]
+pub struct BiosParameterBlock {
+    pub bytes_per_sector: u16,        
+    pub sectors_per_cluster: u8,       
+    pub reserved_sector_count: u16,  
+    pub num_fats: u8,                
+    pub root_entry_count: u16,        
+    pub total_sectors_16: u16,        
+    pub media_descriptor: u8,          
+    pub fat_size_16: u16,             
+    pub sectors_per_track: u16,     
+    pub num_heads: u16,             
+    pub hidden_sectors: u32,        
+    pub total_sectors_32: u32,     
+
+    pub fat_size_32: u32,              
+    pub ext_flags: u16,              
+    pub fs_version: u16,          
+    pub root_cluster: u32,            
+    pub fs_info_sector: u16,           
+    pub backup_boot_sector: u16,       
+    pub reserved: [u8; 12],           
+    pub drive_num: u8,                 
+    pub reserved_1: u8,                
+    pub boot_signature: u8,           
+    pub volume_id: u32,                
+    pub volume_label: [u8; 11],        
+    pub fs_type: [u8; 8],              
+}
+
+#[repr(packed)]
+#[allow(dead_code)]
+pub struct BootSector {
+    pub jmp_boot: [u8; 3],     
+    pub oem_name: [u8; 8],          
+    pub bpb: BiosParameterBlock,    
+    _padding: [u8; 420],            
+    pub boot_signature: u16,        
+}
